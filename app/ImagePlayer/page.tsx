317d7091856d5271b1f1216c44052038d2a72c04
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2024-12-29 18:42:35 +0000

    trying to fix build error ImagePlayerProps IS NOT VALID

--- app/ImagePlayer/page.tsx
+++ app/ImagePlayer/page.tsx
@@ -1,13 +1,46 @@
 "use client";
 import Image from "next/image"; 
-import { ImageItem } from "../types/image";
+// import { ImageItem } from "../types/image";
 
+// type ImagePlayerProps = {
+//   images: ImageItem[];
+//   intervals: number[];
+//   currentImageIndex: number;
+// }
+
+interface ImageItem {
+  id: string;
+  url: string;
+}
+
+
+// I KEEP GETTING THE BUILD ERROR THAT ImagePlayerProps IS NOT VALID...
 type ImagePlayerProps = {
   images: ImageItem[];
   currentImageIndex: number;
 }
 
+// const ImagePlayer = ({ images, intervals, currentImageIndex }: ImagePlayerProps) => {
 const ImagePlayer = ({ images, currentImageIndex }: ImagePlayerProps) => {
+  // so if you are able to receive midi notes, then you I suppose don't need to worry about the interval duration 
+  // leaving below for now commented out, will probably add the possibility to sequence without incoming midi notes too...
+  
+  // ***
+  // const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // const [currentIntervalIndex, setCurrentIntervalIndex] = useState(0);
+  // since it will be determined by the midi note being received
+  // useEffect(() => {
+  //   // Set up an interval to update the current image index
+  //   const timer = setInterval(() => {
+  //     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  //     setCurrentIntervalIndex((prevIndex) => (prevIndex + 1) % intervals.length);
+  //   }, intervals[currentIntervalIndex]);
+
+  //   // Clean up the interval when the component unmounts
+  //   return () => clearInterval(timer);
+  // }, [images.length, currentImageIndex, intervals, currentIntervalIndex]);
+  // ***
+
   if (images.length === 0) return <p className="text-gray-600">No images uploaded yet.</p>;
 
   console.log("currentImageIndex: " + currentImageIndex);
