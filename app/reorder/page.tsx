cb7703dc5f6f8090e835db926f78ceb53ab9916e
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2025-01-04 23:24:57 +0000

    add randomize button for user to reorder images

--- app/reorder/page.tsx
+++ app/reorder/page.tsx
@@ -13,39 +13,51 @@ const Reorder = () => {
   const [ fromIndex, setFromIndex ] = useState<number | null>(null);
   const [ firstClickTurn, setFirstClickTurn ] = useState<boolean>(true);
 
-    // handle the clicks on from and to image cards
-    // so if the image that you are moving is of a higher index than the index that you are moving the image to
-    // then the previous image at that index will increase by 1
-    // and if the image that you are moving is of a lower index than the index that you are moving the image to
-    // then the previous image at that index will decrease by 1
-    const handleImageClick = (id: string, index: number) => {
-      if (firstClickTurn) {
-        setFromIndex(index);  // Set the fromIndex when the first image is clicked
-        setFirstClickTurn(false);  // Set the flag to false after the first click
-      } 
-      else { // 2nd click (move image to index)
-        console.log('else index: ' + index);
-        if (fromIndex !== null && fromIndex !== index) {
-          const updatedImages = [...images]; // make copy of original array
-          const movedItem = updatedImages[fromIndex];
-          // if the very last index of the image array, after removing the element from the from index 
-          // the to index will be out of bounds so decrease by 1
-          if (index == images.length) {
-            updatedImages.splice(fromIndex, 1); // Remove the item from its original position
-            // 1 less if the last index to avoid going out of bounds
-            updatedImages.splice(index - 1, 0, movedItem); // Insert the item at the target position 
-          }
-          else {
-            updatedImages.splice(fromIndex, 1); // Remove the item from its original position
-            updatedImages.splice(index, 0, movedItem); // Insert the item at the target position
-          }
-          setImages(updatedImages); // Reorder the images
+  // handle the clicks on from and to image cards
+  // so if the image that you are moving is of a higher index than the index that you are moving the image to
+  // then the previous image at that index will increase by 1
+  // and if the image that you are moving is of a lower index than the index that you are moving the image to
+  // then the previous image at that index will decrease by 1
+  const handleImageClick = (id: string, index: number) => {
+    if (firstClickTurn) {
+      setFromIndex(index);  // Set the fromIndex when the first image is clicked
+      setFirstClickTurn(false);  // Set the flag to false after the first click
+    } 
+    else { // 2nd click (move image to index)
+      console.log('else index: ' + index);
+      if (fromIndex !== null && fromIndex !== index) {
+        const updatedImages = [...images]; // make copy of original array
+        const movedItem = updatedImages[fromIndex];
+        // if the very last index of the image array, after removing the element from the from index 
+        // the to index will be out of bounds so decrease by 1
+        if (index == images.length) {
+          updatedImages.splice(fromIndex, 1); // Remove the item from its original position
+          // 1 less if the last index to avoid going out of bounds
+          updatedImages.splice(index - 1, 0, movedItem); // Insert the item at the target position 
         }
-        setFirstClickTurn(true);  // Reset flag for next round of clicks
-        setFromIndex(null);  // Reset fromIndex after reordering
+        else {
+          updatedImages.splice(fromIndex, 1); // Remove the item from its original position
+          updatedImages.splice(index, 0, movedItem); // Insert the item at the target position
+        }
+        setImages(updatedImages); // Reorder the images
       }
+      setFirstClickTurn(true);  // Reset flag for next round of clicks
+      setFromIndex(null);  // Reset fromIndex after reordering
+    }
 
-    };
+  };
+
+  // randomize images in images array (no undo available)
+  const randomizeImages = () => {
+    const reorderedImages = [...images];
+    for (let i = images.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // Fisher-Yates Algorithm
+      // swap images, guarantess one-to-one since never removing an image from the array
+      [reorderedImages[i], reorderedImages[j]] = [reorderedImages[j], reorderedImages[i]]; 
+    }
+    setImages(reorderedImages);
+  }
 
   return(
     <div className='bg-gray-900 p-4'>
@@ -55,13 +67,21 @@ const Reorder = () => {
           Click the image that you would like to move <br/>
           Then click the place in the sequence where you want the image to be placed
         </p>
-        <div className='pt-8 text-center'>
-        <Link href="/">
-          <button className="px-8 py-4 bg-blue-500 text-white text-xl rounded hover:bg-blue-600">
-            Home
+        <div className='pt-8 text-center space-x-4'>
+          {/* Home button */}
+          <Link href="/">
+            <button className="px-8 py-4 bg-blue-500 text-white text-xl rounded hover:bg-blue-600">
+              Home
+            </button>
+          </Link>
+          {/* Randomize button */}
+          <button
+            onClick={randomizeImages}
+            className="px-8 py-4 bg-green-500 text-white text-xl rounded hover:bg-green-600"
+          >
+            Randomize
           </button>
-        </Link>
-      </div>
+        </div>
 
       </div>
 
